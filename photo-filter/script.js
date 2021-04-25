415f72f8d981f248faa8c23adaf1ed3014b122a1
AnzhelikaMikulich <angelika.flyorko@gmail.com>
2021-04-25 15:48:48 +0300

    feat:when loading, display a picture according to the time of day

--- photo-filter/script.js
+++ photo-filter/script.js
@@ -1,12 +1,13 @@
-
-
-
-
+const fullscreen = document.querySelector('.openfullscreen');
+const pictures  = document.querySelector('.container-pictures ');
+const base = 'https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/';
+const images = ['01.jpg', '02.jpg', '03.jpg', '05.jpg', '06.jpg', '07.jpg', '08.jpg', '09.jpg', '10.jpg', '11.jpg', '12.jpg', '13.jpg', '14.jpg', '15.jpg', '16.jpg', '17.jpg', '18.jpg', '19.jpg', '20.jpg'];
+let i = 0;
+const next = document.querySelector('.btn-next');
 
 
 
 // кнопка fullscreen
-const fullscreen = document.querySelector('.openfullscreen');
 fullscreen.addEventListener('click', changeScreen);
 
 function changeScreen() {
@@ -16,12 +17,26 @@ function changeScreen() {
 		document.exitFullscreen();
 	}
 }
-// Загрузка изображения согласно времени суток
-const base = 'https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/';
-const images = ['01.jpg', '02.jpg', '03.jpg', '05.jpg', '06.jpg', '07.jpg', '08.jpg', '09.jpg', '10.jpg', '11.jpg', '12.jpg', '13.jpg', '14.jpg', '15.jpg', '16.jpg', '17.jpg', '18.jpg', '19.jpg', '20.jpg'];
-let i = 0;
-const pictures  = document.querySelector('.container-pictures ');
-const next = document.querySelector('.btn-next');
+
+//Загрузка стартового изображения из файлов приложения согласно времени суток
+function viewStartImage() {  
+  const date = new Date();
+  const hour = date.getHours();
+  if (hour >= 6 && hour < 12) {
+    timesDay = './assets/img/morning.jpg';
+} else if (hour >= 12 && hour < 18) {
+    timesDay = './assets/img/day.jpg';
+} else if (hour >= 18 && hour < 24) {
+    timesDay = './assets/img/evening.jpg';
+} else if (hour >= 0 && hour < 6) {
+    timesDay = './assets/img/night.jpg';
+}; 
+    pictures.style.backgroundImage = `url(${timesDay})`;
+ 
+}
+viewStartImage()
+
+// Загрузка изображения Next согласно времени суток
 
 function viewBgImage(src) {  
   const img = new Image();
@@ -53,3 +68,4 @@ let timesDay = '';
   setTimeout(function() { next.disabled = false }, 1000);
 } 
 next.addEventListener('click', getImage);
+
